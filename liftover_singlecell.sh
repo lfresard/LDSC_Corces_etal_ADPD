f8725561d01073f677abdfeff727ad356ad8ce97
lfresard <lfresard@users.noreply.github.com>
2019-09-24 15:02:29 -0700

    Update liftover_singlecell.sh

--- liftover_singlecell.sh
+++ liftover_singlecell.sh
@@ -1,11 +1,11 @@
 # Transform single cell Atac-seq peaks bed files in from hg38 to hg19 
 
 #Idr Optimal Peaks
-atacseq_folder=/users/lfresard/scratch/alzheimers_parkinsons/data/atacseq/kundaje_version/CelltypeSpecificIdrOptimalPeaksBedtoolsMerge
+atacseq_folder=$1
 for x in astrocytes doublets excitatory_neurons inhibitory_neurons microglia neurons_unknown nigral_neurons oligodendrocytes opcs ; do
 echo "Processing ${x}"
 liftOver $atacseq_folder/$x.idr.optimal_peak.narrowPeak.gz  \
-/srv/persistent/bliu2/tools/ucsc_tools/hg38ToHg19.over.chain.gz \
+hg38ToHg19.over.chain.gz \
 $atacseq_folder/$x.idr.optimal_peak.narrowPeak.hg19.bed \
 $atacseq_folder/$x.idr.optimal_peak.narrowPeak.unmapp.bed 
 
