bdea1fe80c2233dc6a0511ebda21a089484faac7
mikegloudemans <mgloud@durga.stanford.edu>
2020-05-13 14:37:45 -0700

    Rearranged file structure more logically

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # LD score regression: Cell type specific analyses
+
+The full order of LDSC analyses are outlined in `pipe.sh`. 
--- run_all.sh
+++ run_all.sh
@@ -3,23 +3,23 @@
 # specific heritability partitioning
 ##################################################
 
-source load_local_env.sh
+source scripts/load_local_env.sh
 
 # Munge summary statistics
-bash gwas_sumstat.sh
+bash scripts/gwas_sumstat.sh
 
 # Lift over ATAC-seq peaks from hg38 to hg19
-bash liftover_singlecell.sh
+bash scripts/liftover_singlecell.sh
 
 # Annotate SNPs by which tissues they belong to
-Rscript tissue_specific_snp_annotation_single_cell.R
+Rscript scripts/tissue_specific_snp_annotation_single_cell.R
 
 # Generate LD scores for each single cell type
-bash ldscore_singlecell.sh
+bash scripts/ldscore_singlecell.sh
 
 # Perform LD score regression to partition heritability
-bash cell_type_spe_regression.sh
+bash scripts/cell_type_spe_regression.sh
 
 # Plot final enrichments
-Rscript plot_cellspecific_enrichments.R
+Rscript scripts/plot_cellspecific_enrichments.R
 
